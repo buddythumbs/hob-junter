c64858a282b8409753e849490a888e28d309bc57
buddythumbs <moleary1987@gmail.com>
2018-12-06 23:37:50 +0000

    Changing to class based component

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 import hash from 'object-hash';
 import List from '@material-ui/core/List';
@@ -7,20 +8,17 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Search from '@material-ui/icons/Search';
 
-const SearchResult = ({job}) => <ListItem >
-    <ListItemText primary={job.suggestion}/>
-    <ListItemIcon>
-        <Search />
-    </ListItemIcon>
-</ListItem>
-
-export default ({results}) => {
-  return <List>
-    {
-        _.map(
-            results,
-            result => <SearchResult key={hash(result)} job={result} />
+export default class SearchResult extends Component {
+    static propTypes = {
+        results: PropTypes.arrayOf(
+            PropTypes.shape({
+                
+            })
         )
     }
-    </List>
+    render = () => {
+        const { results } = this.props;
+
+        return <div>{results.length} resulsts returned</div>
+    }
 }
