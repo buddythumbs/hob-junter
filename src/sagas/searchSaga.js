033182f9e3c449a05972ed6d5f5e729669bad8db
buddythumbs <moleary1987@gmail.com>
2018-12-06 23:39:03 +0000

    Adding search job title saga

--- src/sagas/searchSaga.js
+++ src/sagas/searchSaga.js
@@ -1,12 +1,10 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import {actions} from '../utils/types';
-import {memoize} from '../utils/memoize';
 import {DAW_API} from '../api/data-at-work-api';
 
-const getBeginsWith = memoize(DAW_API.AUTOCOMPLETE)
-
-// worker Saga: will be fired on USER_FETCH_REQUESTED actions
+// worker Saga: will be fired on SET_SEARCH_TEXT action
 function* fetchAutocomplete(action) {
+    if(action.payload === "") return;
     yield put({type: actions.FETCH_KEYWORDS, payload: action.payload});
    try {
       const keywords = yield call(DAW_API.AUTOCOMPLETE, 'START',action.payload);
@@ -16,25 +14,23 @@ function* fetchAutocomplete(action) {
    }
 }
 
-/*
-  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
-  Allows concurrent fetches of user.
-*/
-// function* mySaga() {
-//   yield takeEvery("USER_FETCH_REQUESTED", fetchUser);
-// }
-
-/*
-  Alternatively you may use takeLatest.
-
-  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
-  dispatched while a fetch is already pending, that pending fetch is cancelled
-  and only the latest one will be run.
-*/
-function* searchSaga() {
-    console.log();
-    
+// worker Saga: will be fired on SET_SEARCH_TEXT action
+function* fetchJobs(action) {
+  if(action.payload === "") return;
+  yield put({type: actions.FETCH_JOBS, payload: action.payload});
+  try {
+    const jobs = yield call(DAW_API.SEARCH_JOBS_BY_ID, action.payload);
+    yield put({type: actions.FETCH_JOBS_SUCCEEDED, payload: jobs.data});
+  } catch (e) {
+    yield put({type: actions.FETCH_JOBS_FAILED, message: e.message});
+  }
+}
+// Intercept SET_SEARCH_TEXT action type
+export function* searchKeywordSaga() {
   yield takeLatest(actions.SET_SEARCH_TEXT, fetchAutocomplete);
 }
 
-export default searchSaga;
+// Intercept SET_SEARCH_JOB_ID action type
+export function* searchJobSaga() {
+  yield takeLatest(actions.SEARCH_JOB_ID, fetchJobs);
+}
