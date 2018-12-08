426caf58840f22d43864a450e1c6cbdba1146c1c
buddythumbs <moleary1987@gmail.com>
2018-12-08 23:09:02 +0000

    Better error handling for no entries fround

--- src/sagas/searchSaga.js
+++ src/sagas/searchSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, throttle } from 'redux-saga/effects';
 import {actions} from '../utils/types';
 import {DAW_API} from '../api/data-at-work-api';
 
@@ -10,7 +10,11 @@ function* fetchAutocomplete(action) {
       const keywords = yield call(DAW_API.AUTOCOMPLETE, 'START',action.payload);
       yield put({type: actions.FETCH_KEYWORDS_SUCCEEDED, payload: keywords.data});
    } catch (e) {
-      yield put({type: actions.FETCH_KEYWORDS_FAILED, message: e.message});
+      if(e.response){
+        yield put({type: actions.FETCH_KEYWORDS_FAILED, payload: e.response.data.error.message});
+      }else{
+        yield put({type: actions.FETCH_KEYWORDS_FAILED, payload: e.message});
+      }
    }
 }
 
@@ -20,9 +24,14 @@ function* fetchJobs(action) {
   yield put({type: actions.FETCH_JOBS, payload: action.payload});
   try {
     const jobs = yield call(DAW_API.SEARCH_JOBS_BY_ID, action.payload);
-    yield put({type: actions.FETCH_JOBS_SUCCEEDED, payload: jobs.data});
+    const related_skills = yield call(DAW_API.SEARCH_SKILLS_BY_JOB_ID, action.payload);
+    yield put({ type: actions.FETCH_JOBS_SUCCEEDED, payload: {...jobs.data, ...related_skills.data}});
   } catch (e) {
-    yield put({type: actions.FETCH_JOBS_FAILED, message: e.message});
+    if(e.response){
+      yield put({type: actions.FETCH_JOBS_FAILED, payload: e.response.data.error.message});
+    }else{
+      yield put({type: actions.FETCH_JOBS_FAILED, payload: e.message});
+    }
   }
 }
 // Intercept SET_SEARCH_TEXT action type
