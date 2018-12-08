defee1677f2dfe62d6f1e1cfa4893f56bceae3af
buddythumbs <moleary1987@gmail.com>
2018-12-08 20:13:49 +0000

    Adding skills and changing layout

--- src/components/JobDetail/JobTypeDetail.jsx
+++ src/components/JobDetail/JobTypeDetail.jsx
@@ -9,6 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
+import { DetailRow, DetailColumn } from '../../elements/layouts';
 
 
 class JobTypeDetail extends Component {
@@ -22,6 +23,17 @@ class JobTypeDetail extends Component {
           PropTypes.shape({
             uuid: PropTypes.string.isRequired,
             title: PropTypes.string.isRequired
+          })
+        ),
+        skills: PropTypes.arrayOf(
+          PropTypes.shape({
+            skill_uuid: PropTypes.string.isRequired,
+            skill_name: PropTypes.string.isRequired,
+            skill_type: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            normalized_skill_name: PropTypes.string.isRequired,
+            importance: PropTypes.number.isRequired,
+            level: PropTypes.number.isRequired
           })
         ),
         title: PropTypes.string.isRequired,
@@ -41,22 +53,40 @@ class JobTypeDetail extends Component {
         return (
           <Card >
             <CardContent>
-              <Typography variant="h5" component="h2">
-                {fetchedJobDetail.title}
-              </Typography>
-              <Typography component="p">
-                {fetchedJobDetail.description}
-              </Typography>
-              <CardActions>
-                <Button variant="contained" color="primary">Show me these jobs!</Button>
-              </CardActions>
-              <Typography variant="h5" component="h2">
-                Related Titles
-              </Typography>
-              {_.map(
-                fetchedJobDetail.related_job_titles,
-                title => <Chip color="primary" key={title.uuid} label={title.title} style={{marginLeft: 8}} onClick={() => searchJob(title.uuid) } variant="outlined" />
-              )}
+              <DetailColumn>
+                <Typography style={{padding: 10}} variant="p" component="h2">
+                  {fetchedJobDetail.title}
+                </Typography>
+                <Typography style={{padding: 10}} component="p">
+                  {fetchedJobDetail.description}
+                </Typography>
+              </DetailColumn>
+              <DetailColumn style={{padding: 10}} >
+                <Typography variant="h6" component="h2">
+                  Related Titles
+                </Typography>
+                  <DetailRow>
+                    {_.map(
+                      _.take(
+                        fetchedJobDetail.related_job_titles,
+                        5
+                        ),
+                      title => <Chip color="primary" key={title.uuid} label={title.title} style={{margin: 8}} variant="outlined" />
+                    )}
+                  </DetailRow>
+                <Typography variant="h6" component="h2">
+                  Related Skills
+                </Typography>
+                  <DetailRow>
+                    {_.map(
+                      _.take(
+                        fetchedJobDetail.skills,
+                        10
+                        ),
+                      skill => <Chip color="secondary" key={skill.skill_uuid} label={skill.skill_name} style={{margin: 8}} variant="outlined" />
+                    )}
+                  </DetailRow>
+              </DetailColumn>
             </CardContent>
           </Card>
         );
