53f34d034e035cb723381ecd825de811432a701d
buddythumbs <moleary1987@gmail.com>
2018-12-08 23:09:27 +0000

    Updating reducer to take error messages

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -4,23 +4,52 @@ import { actions } from '../utils/types';
 // use constant for initial state in order to reset if needed
 const initialState = {
     searchText: '',
-    loadingAutocomplete: false
+    loadingAutocomplete: false,
+    fetchedJob: false,
 }
 
 const search = (state=initialState,action) => {
     switch (action.type) {
         case actions.SET_SEARCH_TEXT:
             return state = { ...state, searchText: action.payload }
+        case actions.SEARCH_JOB_TITLE:
+            return state = { ...state, searchTitle: action.payload }
         case actions.CLEAR_SEARCH:
-            return state = { ...state, searchText: '' }
+            return state = { ...state, searchText: '', searchResults: undefined }
         case actions.FETCH_KEYWORDS:
-            return state = { ...state, loadingAutocomplete: true }
+            return state = { 
+                ...state, 
+                loadingAutocomplete: true,
+                errorMessage: '', 
+                error: false 
+            }
         case actions.FETCH_KEYWORDS_SUCCEEDED:
-            return state = { ...state, searchResults: action.payload, loadingAutocomplete: false }
+            return state = { 
+                ...state, 
+                searchResults: action.payload, 
+                loadingAutocomplete: false 
+            }
+        case actions.FETCH_KEYWORDS_FAILED:
+            return state = { 
+                ...state, 
+                loadingAutocomplete: false, 
+                errorMessage: action.payload, 
+                error: true 
+            }
         case actions.FETCH_JOBS:
-            return state = { ...state, fetchedJob: undefined, fetchedJob: false }
+            return state = { 
+                ...state, 
+                fetchedJob: undefined, 
+                fetchedJob: false, 
+                errorMessage: action.payload, 
+                error: true 
+            }
         case actions.FETCH_JOBS_SUCCEEDED:
-            return state = { ...state, fetchedJobDetail: action.payload, fetchedJob: true }
+            return state = { 
+                ...state, 
+                fetchedJobDetail: action.payload, 
+                fetchedJob: true, 
+            }   
         default:
             return state
     }
