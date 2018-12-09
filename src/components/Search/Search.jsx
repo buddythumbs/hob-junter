6b6eb838fdb350a9e8de3803f0c40cadb2b78522
buddythumbs <moleary1987@gmail.com>
2018-12-09 12:40:37 +0000

    Add linear progress

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,6 +7,7 @@ import SearchResults from './SearchResults';
 import AutoComplete from './AutoComplete';
 import JobDetail from '../JobDetail/JobTypeDetail';
 import { Main } from '../../elements/layouts';
+import { LinearProgress } from '@material-ui/core';
 
 export default class Search extends Component {
   static propTypes = {
@@ -24,37 +25,7 @@ export default class Search extends Component {
       errorMessage: PropTypes.string,
     }).isRequired,
     setSearchText: PropTypes.func.isRequired,
-    searchId: PropTypes.func.isRequired,
-    fetchedJobDetail: PropTypes.shape({
-      description: PropTypes.string.isRequired,
-      onet_soc_code: PropTypes.string.isRequired,
-      related_job_titles: PropTypes.arrayOf(
-        PropTypes.shape({
-          uuid: PropTypes.string.isRequired,
-          title: PropTypes.string.isRequired
-        })
-      ),
-      skills: PropTypes.arrayOf(
-        PropTypes.shape({
-          skill_uuid: PropTypes.string.isRequired,
-          skill_name: PropTypes.string.isRequired,
-          skill_type: PropTypes.string.isRequired,
-          description: PropTypes.string.isRequired,
-          normalized_skill_name: PropTypes.string.isRequired,
-          importance: PropTypes.number.isRequired,
-          level: PropTypes.number.isRequired
-        })
-      ),
-      title: PropTypes.string.isRequired,
-      unusual_job_titles: PropTypes.arrayOf(
-        PropTypes.shape({
-          uuid: PropTypes.string.isRequired,
-          title: PropTypes.string.isRequired
-        })
-      ),
-      uuid: PropTypes.string.isRequired
-    })
-
+    searchId: PropTypes.func.isRequired
   }
 
   handleChangeText = (e,type) => {
@@ -63,18 +34,18 @@ export default class Search extends Component {
 
   render() {
     
-    const { search: { error, errorMessage, searchText, searchResults, fetchedJob, fetchedJobDetail, loadingAutocomplete } } = this.props;
+    const { search: { error, errorMessage, searchText, searchResults, loadingAutocomplete } } = this.props;
 
     return <Main>
       <h4>What do you want to be?</h4>
       <KeyWordSearch value={searchText} onChange={this.handleChangeText} />
+      {error && <h5>{errorMessage}</h5>}
+      {loadingAutocomplete && <LinearProgress />}
       {searchResults && searchResults.length && 
         <SearchResults 
           searchText={searchText}
           results={searchResults} 
           loading={loadingAutocomplete} 
-          error={error} 
-          errorMessage={errorMessage}
           goTo={this.props.goTo}
         />
       }
