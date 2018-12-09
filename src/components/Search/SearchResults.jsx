85e9ecac58c6e5fed702614ddb984824c60cc520
buddythumbs <moleary1987@gmail.com>
2018-12-09 12:39:44 +0000

    Tidying up unused comoponents

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -1,12 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+
+// Components
 import Chip from '@material-ui/core/Chip';
-import Open from '@material-ui/icons/OpenInNew';
 
 export default class SearchResult extends Component {
     static propTypes = {
@@ -18,8 +15,6 @@ export default class SearchResult extends Component {
         ),
         searchText: PropTypes.string.isRequired,
         loadingAutocomplete: PropTypes.bool.isRequired,
-        error: PropTypes.bool,
-        errorMessage: PropTypes.string,
         goTo: PropTypes.func.isRequired,
     }
 
@@ -31,16 +26,16 @@ export default class SearchResult extends Component {
                 {_.map(
                     results,
                     (result,i) => <Chip
-                    key={i}
-                    style={{margin: 3}}
-                    label={result.suggestion}
-                    onClick={() => goTo(`jobs/${result.parent_uuid}`)} 
-                    variant="outlined"
-                    color='secondary'
+                        key={i}
+                        style={{margin: 3}}
+                        label={result.suggestion}
+                        onClick={() => goTo(`jobs/${result.parent_uuid}`)} 
+                        variant="outlined"
+                        color='secondary'
                     />
                     )}
             </div>
-            <div>{results.length} titles sarting with "{searchText}" </div>
+            <h5>{results.length} titles sarting with "{searchText}" </h5>
         </Fragment>
     }
 }
